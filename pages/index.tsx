a4c8b38fc843461e4eddd08652423506a06afa54
Emanuele Ianni <dierre@gmail.com>
2020-11-01 12:56:17 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home({ allPostsData }) {
           <a href="p/dont-make-me-think">The notes on the book <i>Don't make me think (revisited)</i></a>
         </p>
         <p>
-          <a href="p/elemprog/01">Slides for Elementary Programming held at DTU (Copenhagen) on the Winter 2020 Semester</a>
+          <a href="https://github.com/invasionofsmallcubes/elementary-programming-dtu/tree/master/presentations">Slides for Elementary Programming held at DTU (Copenhagen) on the Winter 2020 Semester</a>
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
