eb40cee0a3748f5dc89c778ff0c7f59c6fc7ef30
Emanuele Ianni <dierre@gmail.com>
2021-03-08 14:09:34 +0100

    new tracking

--- components/layout.tsx
+++ components/layout.tsx
@@ -29,7 +29,7 @@ export default function Layout({
                 />
                 <meta name="og:title" content={siteTitle}/>
                 <meta name="twitter:card" content="summary_large_image"/>
-                <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"dierre@gmail.com",utcoffset:"1"}))};sessionStorage.setItem("_swa","1");</script>
+                <script src="tracking.js"></script>
             </Head>
             <header className={styles.header}>
                 {home ? (
